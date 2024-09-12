fb3ab8e9d50dc0443e859182c56447ee8b0cb6c4
advaohana <adva1230@gmail.com>
2024-09-12 19:55:47 +0300

    created routes for users

--- back/routers/users.js
+++ back/routers/users.js
@@ -1,7 +1,29 @@
 import { Router } from "express"
 
 export const usersRouter = Router()
+const tovitRouter = Router({ mergeParams: true })
+const groupsRouter = Router({ mergeParams: true })
 
-usersRouter.get('/:id', (req, res) => {
+usersRouter.get('/all', async (req, res) => { })
+usersRouter.get('/:id', async (req, res) => { })
+usersRouter.get('/byName/:id', async (req, res) => { })
+//התחברות
+usersRouter.post('/sign-up', async (req, res) => { })
+usersRouter.post('/login', async (req, res) => { })
+usersRouter.post('/logout/:id', async (req, res) => { })
+usersRouter.post('/forgot', async (req, res) => { })
+usersRouter.patch('/:id', async (req, res) => { })
+usersRouter.delete('/:id', async (req, res) => { })
 
-})
+usersRouter.use('/:usersId/tovit', tovitRouter)
+tovitRouter.get('/all', async (req, res) => { })
+tovitRouter.get('/:id', async (req, res) => { })
+tovitRouter.post('/', async (req, res) => { })
+tovitRouter.patch('/:id', async (req, res) => { })
+tovitRouter.delete('/:id', async (req, res) => { })
+
+usersRouter.use('/:usersId/group')
+groupsRouter.get('/all', async (req, res) => { })
+groupsRouter.post('/', async (req, res) => { })
+groupsRouter.patch('/:id', async (req, res) => { })
+groupsRouter.delete('/:id', async (req, res) => { })
