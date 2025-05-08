a088c5ccdafd9d3eef7221806e5dbc8d7c0bbb6e
Shmuel atar <shmuelatar@gmail.com>
2025-05-08 19:39:18 +0300

    Finished Verify reset code in frontend Created the verify page and the fetchs to the backend to reset password, added optional chaining to prevent bugs

--- back/public/js/verifyPage.js
+++ back/public/js/verifyPage.js
@@ -5,45 +5,50 @@ async function handleSubmit(event) {
     const code = Array.from(inputs, input => input.value).join("")
     if (code.length !== 6) window.helpers.toaster("Code needs to be 6 digits", "fail")
 
-
-    //handle Fetch to the server with the code to the route of verifyCode "api/users/verifyCode"
-    //if the code is not valid let him keep try, do not redirect him !
-    //if the is wrong the inputs should be cleared
+    const params = new URLSearchParams(window.location.search);
+    params.set('UFC', code)
+    const res = await fetch(`api/users/verifyCode?${params.toString()}`)
+    const data = await res.json();
+    console.log(data);
 
 }
 
 
 
 
+if (window.location.pathname.toLocaleLowerCase() === '/codeverify') {
+    document.addEventListener("DOMContentLoaded", () => {
+        const form = document.querySelector('.forgot-form');
+        form.addEventListener('submit', handleSubmit)
 
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector('.forgot-form');
-    form.addEventListener('submit', handleSubmit)
+        const inputs = document.querySelectorAll('input')
+        inputs.forEach((input, i) => {
+            input.addEventListener('keydown', function (e) {
+                if (/^\d$/.test(e.key)) {
+                    e.preventDefault();
+                    input.value = e.key;
 
-    const inputs = document.querySelectorAll('input')
-    inputs.forEach((input, i) => {
-        input.addEventListener('keydown', function (e) {
-            if (/^\d$/.test(e.key)) {
-                e.preventDefault();
-                input.value = e.key;
+                    if (i < inputs.length - 1) {
+                        inputs[i + 1].focus();
+                    }
+                } else if (e.key === "Backspace") {
+                    e.preventDefault();
 
-                if (i < inputs.length - 1) {
-                    inputs[i + 1].focus();
+                    input.value = "";
+                    if (i > 0) {
+                        inputs[i - 1].focus();
+                    }
                 }
-            } else if (e.key === "Backspace") {
-                e.preventDefault();
-
-                input.value = "";
-                if (i > 0) {
-                    inputs[i - 1].focus();
+                if (!/^\d$/.test(e.key) && e.key !== 'Enter') {
+                    e.preventDefault()
+                    input.value = ""
                 }
-            }
-            if (!/^\d$/.test(e.key) && e.key !== 'Enter') {
-                e.preventDefault()
-                input.value = ""
-            }
 
+            })
         })
-    })
 
-})
+
+    })
+}
+
+
